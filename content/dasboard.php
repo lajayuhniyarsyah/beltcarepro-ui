141aa59768eb078a62d519a6e11c39ccfc46dc8b
ekachandrasetiawan <ekachandrasetiawan@yahoo.com>
2016-06-01 16:38:36 +0700

    Dashboard Responsive #33
    Dashboard Responsive

--- content/dasboard.php
+++ content/dasboard.php
@@ -13,8 +13,9 @@
 	</div>
 </div>
 
-<div class="row">
-<div class="beltcarepro-conv-dash-state col-sm-3">
+
+
+<div class="col-md-3">
   <div class="xe-widget xe-counter xe-counter-red" data-count=".num" data-from="0" data-to="85.3" data-suffix="%" data-duration="2">
     <div class="xe-icon">
       <i class="linecons-fire"></i>
@@ -25,9 +26,9 @@
     </div>
   </div>
 </div>
-<div class="col-sm-3">
+<div class="col-md-3">
   
-  <div class="xe-widget xe-counter xe-counter-blue" data-count=".num" data-from="1" data-to="15" data-suffix="Unit(s)" data-duration="3" data-easing="false">
+  <div class="xe-widget xe-counter xe-counter-blue" data-count=".num" data-from="1" data-to="15" data-suffix="Unit" data-duration="3" data-easing="false">
     <div class="xe-icon">
       <i class="linecons-user"></i>
     </div>
@@ -38,7 +39,7 @@
   </div>
 </div>
 
-<div class="beltcarepro-conv-dash-state col-sm-3">
+<div class="col-md-3">
   
   <div class="xe-widget xe-counter xe-counter-info" data-count=".num" data-from="0" data-to="100" data-duration="4" data-suffix="%" data-easing="true">
     <div class="xe-icon">
@@ -51,7 +52,7 @@
   </div>
 </div>
 
-<div class="beltcarepro-conv-dash-state col-sm-3">
+<div class="col-md-3">
   
   <div class="xe-widget xe-counter xe-counter-red"  data-count=".num" data-from="0" data-to="57" data-prefix="-," data-suffix="%" data-duration="5" data-easing="true" data-delay="1">
     <div class="xe-icon">
@@ -65,8 +66,13 @@
 </div>
 
 
+
+
+
+<div class="row">
+
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
       <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
         <div class="xe-background">
           <i class="linecons-paper-plane"></i>
@@ -97,7 +103,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-green"  data-count=".num" data-from="0" data-to="93.7" data-suffix="%" data-duration="3">
 	    
@@ -130,7 +136,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-orange"  data-count=".num" data-from="0" data-to="75.3" data-suffix="%" data-duration="3">
 	    
@@ -163,7 +169,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-yellow"  data-count=".num" data-from="0" data-to="69.9" data-suffix="%" data-duration="3">
 	    
@@ -196,7 +202,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-red"  data-count=".num" data-from="0" data-to="55.2" data-suffix="%" data-duration="3">
 	    
@@ -229,7 +235,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-red"  data-count=".num" data-from="0" data-to="40.3" data-suffix="%" data-duration="3">
 	    
@@ -261,7 +267,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-orange"  data-count=".num" data-from="0" data-to="75.1" data-suffix="%" data-duration="3">
 	    
@@ -293,7 +299,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="99.8" data-suffix="%" data-duration="3">
 	    
@@ -325,7 +331,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-yellow"  data-count=".num" data-from="0" data-to="70.1" data-suffix="%" data-duration="3">
 	    
@@ -357,7 +363,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
 	    
@@ -389,7 +395,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
 	    
@@ -421,7 +427,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
 	    
@@ -453,7 +459,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
 	    
@@ -485,7 +491,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
 	    
@@ -517,7 +523,7 @@
 </a>
 
 <a href="home.php?page=detail_conveyor_condition">
-	<div class="beltcarepro-conv-dash-state col-sm-3">
+	<div class="beltcarepro-conv-dash-state col-md-3">
 	  
 	  <div class="xe-widget xe-progress-counter xe-progress-counter-turquoise"  data-count=".num" data-from="0" data-to="100" data-suffix="%" data-duration="3">
 	    
